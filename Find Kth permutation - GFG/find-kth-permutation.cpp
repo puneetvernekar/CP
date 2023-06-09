1b66e1027328966cbfb7a8f3577703561936ef1b
puneetvernekar <98655857+puneetvernekar@users.noreply.github.com>
2023-06-09 15:34:20 +0530

    Added solution - LeetHub

--- Find Kth permutation - GFG/find-kth-permutation.cpp
+++ Find Kth permutation - GFG/find-kth-permutation.cpp
@@ -14,6 +14,30 @@ public:
     string kthPermutation(int n, int k)
     {
         // code here
+        //OPTIMAL
+        vector<int>num;
+        int fact=1;
+        for(int i=1;i<n;i++)
+        {
+            fact*=i;
+            num.push_back(i);
+        }
+        num.push_back(n);
+        k=k-1;  // AS WE ARE USING ZERO BASED INDEXING
+        string ans="";
+        while(true)
+        {
+            int row=k/fact;
+            ans+=to_string(num[row]);
+            num.erase(num.begin()+row);
+            if(num.size()==0)
+            break;
+            k=k%fact;
+            fact=fact/num.size();
+        }
+        return ans;
+        
+        /*
         vector<int>str;
         for(int i=1;i<=n;i++)
         str.push_back(i);
@@ -22,7 +46,7 @@ public:
         string ans="";
         for(int i=0;i<n;i++)
         ans+=str[i]+'0';
-        return ans;
+        return ans;*/
     }
 };
 

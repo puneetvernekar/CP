576ac6332547d1b59db692b6045bb806b98cc9b6
puneetvernekar <98655857+puneetvernekar@users.noreply.github.com>
2023-06-11 12:32:33 +0530

    Time: 2388 ms (5.22%), Space: 5.8 MB (98.09%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==1)
+        return true; 
+        for(long i=2;i<num;i++)
+        {
+            if(i*i==num)
+            return true;
+        }
+        return false;
+    }
+};

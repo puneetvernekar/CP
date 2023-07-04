eb411e605762e20620a6eac00f7a2e4001550ef0
puneetvernekar <98655857+puneetvernekar@users.noreply.github.com>
2023-07-04 07:21:22 +0530

    Time: 14 ms (20.77%), Space: 9.4 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int i=1;
-        if(nums.size()<3)
-        return nums[0];
-        sort(nums.begin(),nums.end());
-        while(i<nums.size())
+        // int i=1;
+        // if(nums.size()<3)
+        // return nums[0];
+        // sort(nums.begin(),nums.end());
+        // while(i<nums.size())
+        // {
+        //     if(nums[i]!=nums[i-1])
+        //     return nums[i-1];
+        //     i=i+3;
+        // }
+        // return nums[nums.size()-1];
+        
+        int ans=0;
+        for(int i=0;i<32;i++)
         {
-            if(nums[i]!=nums[i-1])
-            return nums[i-1];
-            i=i+3;
+            int sum=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                sum+=(nums[j]>>i)&1;
+            }
+            sum=sum%3;
+            ans|=(sum<<i);
         }
-        return nums[nums.size()-1];
+        return ans;
     }
 };

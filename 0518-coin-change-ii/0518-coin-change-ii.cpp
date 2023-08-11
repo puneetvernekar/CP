13fbad8c7ba16ab9d3ee4af041cf2d660e25436f
puneetvernekar <98655857+puneetvernekar@users.noreply.github.com>
2023-08-11 21:10:56 +0530

    Time: 41 ms (40.92%), Space: 18.6 MB (29.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         vector<vector<int>> ans(coins.size()+1,vector<int>(amount+1));
-        for(int i=0;i<=amount;i++)
-        ans[0][i]=0;
         for(int i=0;i<=coins.size();i++)
-        ans[i][0]=1;
+        for(int j=0;j<=amount;j++)
+        {   
+            if(i==0)
+            ans[i][j]=0;
+            if(j==0)
+            ans[i][j]=1;
+        }
         for(int i=1;i<=coins.size();i++)
         for(int j=1;j<=amount;j++)
-        {if(j>=coins[i-1])
-        ans[i][j]=ans[i][j-coins[i-1]]+ans[i-1][j];
-        else
-        ans[i][j]=ans[i-1][j];}
+        {   
+            if(j>=coins[i-1])
+            ans[i][j]=ans[i][j-coins[i-1]]+ans[i-1][j];
+            else
+            ans[i][j]=ans[i-1][j];
+        }
         return ans[coins.size()][amount];
 
     }

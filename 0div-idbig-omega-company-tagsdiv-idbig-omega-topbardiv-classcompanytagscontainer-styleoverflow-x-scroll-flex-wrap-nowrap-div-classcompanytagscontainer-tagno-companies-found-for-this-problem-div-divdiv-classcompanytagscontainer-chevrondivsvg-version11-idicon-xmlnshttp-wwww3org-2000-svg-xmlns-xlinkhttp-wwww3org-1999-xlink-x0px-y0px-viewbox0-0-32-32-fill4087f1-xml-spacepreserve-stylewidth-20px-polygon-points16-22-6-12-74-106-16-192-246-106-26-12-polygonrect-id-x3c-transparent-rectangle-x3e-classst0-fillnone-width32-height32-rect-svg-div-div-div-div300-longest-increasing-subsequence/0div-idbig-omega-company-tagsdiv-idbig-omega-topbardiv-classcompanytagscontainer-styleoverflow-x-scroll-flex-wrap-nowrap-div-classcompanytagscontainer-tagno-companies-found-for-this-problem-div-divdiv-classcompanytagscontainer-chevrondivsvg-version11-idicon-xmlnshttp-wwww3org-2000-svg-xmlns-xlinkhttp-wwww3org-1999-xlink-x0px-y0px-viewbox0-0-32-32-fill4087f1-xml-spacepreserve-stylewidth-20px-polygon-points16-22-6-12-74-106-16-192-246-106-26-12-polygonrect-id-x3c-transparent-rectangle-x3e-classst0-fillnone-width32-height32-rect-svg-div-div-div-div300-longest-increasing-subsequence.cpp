12fc2afbb9ee358ca9ec190a8b6986ee9a3f6ff2
puneetvernekar <98655857+puneetvernekar@users.noreply.github.com>
2024-01-15 17:05:02 +0530

    Time: 241 ms (46.92%), Space: 10.8 MB (55.84%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
-    int dp[2501][2501];
-    int solve(int i,int p,vector<int>&nums)
-    {
-        if(i>=nums.size())
-            return 0;
-        if(p!=-1 && dp[i][p]!=-1)
-            return dp[i][p];
-        int take=0;
-        if(p==-1 || nums[i]>nums[p])
-        {
-            //int prev=nums[i];
-            take=1+solve(i+1,i,nums);    
-        }
-        int skip=solve(i+1,p,nums);
+//     int dp[2501][2501];
+//     int solve(int i,int p,vector<int>&nums)
+//     {
+//         if(i>=nums.size())
+//             return 0;
+//         if(p!=-1 && dp[i][p]!=-1)
+//             return dp[i][p];
+//         int take=0;
+//         if(p==-1 || nums[i]>nums[p])
+//         {
+//             //int prev=nums[i];
+//             take=1+solve(i+1,i,nums);    
+//         }
+//         int skip=solve(i+1,p,nums);
         
-        if(p!=-1)
-            dp[i][p]=max(take,skip);
+//         if(p!=-1)
+//             dp[i][p]=max(take,skip);
         
-        return max(take,skip);
-    }
+//         return max(take,skip);
+//     }
     
     int lengthOfLIS(vector<int>& nums) {
         
-        int prev=-1;
-        memset(dp,-1,sizeof(dp));
-        return solve(0,prev,nums);
+        // int prev=-1;
+        // memset(dp,-1,sizeof(dp));
+        // return solve(0,prev,nums);
+        
+        int n=nums.size();
+        vector<int>dp(n,1);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+        
     }
 };
